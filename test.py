0ac703eb856b32234228d851fe991730511d2dbf
victor ibikunle <114394031+VictorIbikunle@users.noreply.github.com>
2023-11-07 19:17:38 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+from keras.models import model_from_json
+
+#using a scale of 0-6 to predict the emotions
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+# Load the emotion detection model
+json_file = open('model/emotion_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+emotion_model = model_from_json(loaded_model_json)
+emotion_model.load_weights("model/emotion_model.h5")
+print("Loaded model from disk")
+
+# Start the webcam feed
+cap = cv2.VideoCapture(0)
+
+# Creating and saving the video
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))  # Adjust the resolution and frame rate as needed
+
+while True:
+    # Find the haar cascade to draw a bounding box around the face
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Resize the frame
+    frame = cv2.resize(frame, (640, 480))
+
+    # Detect faces in the frame
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascades_frontalface_default.xml') #cropping the face images and storing them 
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+
+        # Predict the emotion
+        emotion_prediction = emotion_model.predict(cropped_img)
+        maxindex = int(np.argmax(emotion_prediction))
+        cv2.putText(frame, emotion_dict[maxindex], (x+5, y-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    # Write the frame to the video file
+    out.write(frame)
+
+    cv2.imshow('Emotion Detection', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and writer objects
+cap.release()
+out.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
